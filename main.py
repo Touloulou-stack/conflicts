d1385e1d7674a9422d03446b98839d47a3655d53
carl1 <carl1@gmail.com>
2022-08-25 14:01:40 +0200

    Edited main.py

--- main.py
+++ main.py
@@ -8,12 +8,24 @@ class Graph:
     def add_edge(self, u, v):
         self.graph[u].append(v)
  
-    def __dfs_util(self, v, visited):
+    class Graph:
+    
+    def __init__(self):
+        self.graph = defaultdict(list)
+ 
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+ 
+    def dfs_rec(self, v, visited):
         visited.add(v)
         print(v, end=' ')
         for neighbour in self.graph[v]:
             if neighbour not in visited:
-                self.__dfs_util(neighbour, visited)
+                self.dfs_rec(neighbour, visited)
+
+    def dfs(self, v):
+        visited = set()
+        self.dfs_rec(v, visited)
 
     def dfs(self, v):
         visited = set()
